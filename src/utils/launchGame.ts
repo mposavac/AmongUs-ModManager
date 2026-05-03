ba3fc7902889f3bfcb9b0062b53feb740063fbc0
Matej Posavac <matejposavac27@gmail.com>
2026-05-03 10:56:30 +0200

    Fix launch

--- src/utils/launchGame.ts
+++ src/utils/launchGame.ts
@@ -3,7 +3,6 @@ import { exec, execSync } from "child_process";
 import { BrowserWindow } from "electron";
 
 export const launchGame = async () => {
-  console.log("TEST");
   const bclPath = path.join(
     process.env.LOCALAPPDATA,
     "Programs",
@@ -11,14 +10,10 @@ export const launchGame = async () => {
     "Better-CrewLink.exe",
   );
   try {
-    exec(`"${bclPath}"`, (error) => {
-      if (error) {
-        console.error(`Launch error: ${error}`);
-      } else {
-        execSync("start steam://launch/945360");
-        const win = BrowserWindow.getFocusedWindow();
-        if (win) win.minimize();
-      }
+    exec(`"${bclPath}"`, () => {
+      execSync("start steam://launch/945360");
+      const win = BrowserWindow.getFocusedWindow();
+      if (win) win.minimize();
     });
 
     return { success: true };
